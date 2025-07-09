f581c32e2b7dfd6e27fb92dd810ff38b3bbff1cf
Daniel Woo <woodaniel45@hotmail.com>
2025-07-09 17:40:40 -0300

    Atualização

--- SelecionandoCandidato/selecionandocandidato.java
+++ SelecionandoCandidato/selecionandocandidato.java
@@ -3,50 +3,76 @@ package SelecionandoCandidato;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 import javax.swing.JOptionPane;
 
 public class selecionandocandidato {
-    
-    public static void main(String[] args) {
-         System.out.println("----------- PROCESSO SELETIVO SALÁRIO ---------------");
-        
-        // Parte 1: Entrada de 8 nomes
-        String[] nomes = new String[8];
 
-        for (int i = 0; i < nomes.length; i++) {
+    public static void main(String[] args) {
+        System.out.println("----------- PROCESSO SELETIVO SALÁRIO ---------------");
+
+        // Parte 1: Coletar nomes dos candidatos
+        String[] nomes = coletarNomes(8);
+
+        // Parte 2: Selecionar 5 candidatos aleatoriamente
+        List<String> selecionados = selecionarCandidatos(nomes, 5);
+
+        // Parte 3: Exibir candidatos selecionados
+        exibirSelecionados(selecionados);
+
+        // Parte 4: Ligar para candidatos selecionados
+        processarLigacoes(selecionados);
+    }
+
+    // Parte 1
+    public static String[] coletarNomes(int quantidade) {
+        String[] nomes = new String[quantidade];
+        for (int i = 0; i < quantidade; i++) {
             nomes[i] = JOptionPane.showInputDialog("Digite o nome " + (i + 1) + ":");
         }
+        return nomes;
+    }
 
-        // Parte 2: Sorteio de 5 nomes aleatórios, sem repetição
+    // Parte 2
+    public static List<String> selecionarCandidatos(String[] nomes, int quantidade) {
         List<String> selecionados = new ArrayList<>();
         Random random = new Random();
 
-        while (selecionados.size() < 5) {
+        while (selecionados.size() < quantidade) {
             String nome = nomes[random.nextInt(nomes.length)];
             if (!selecionados.contains(nome)) {
                 selecionados.add(nome);
             }
         }
 
-        // Parte 3: Exibição dos nomes selecionados
+        return selecionados;
+    }
+
+    // Parte 3
+    public static void exibirSelecionados(List<String> selecionados) {
         JOptionPane.showMessageDialog(null, "Nomes Selecionados:");
         for (String nome : selecionados) {
             JOptionPane.showMessageDialog(null, nome);
         }
+    }
 
+    // Parte 4
+    public static void processarLigacoes(List<String> selecionados) {
+        for (String candidato : selecionados) {
+            String valorStr = JOptionPane.showInputDialog(
+                "Candidato: " + candidato + "\nDigite o valor que gostaria de receber:"
+            );
+            int valorDesejado = Integer.parseInt(valorStr);
 
-        // Parte 4: Entrada de nome e valor desejado
-        String n1 = JOptionPane.showInputDialog("Digite o Valor que Gostaria de Receber:");
-        int n2 = Integer.parseInt(n1);
+            if (valorDesejado >= 2000) {
+                JOptionPane.showMessageDialog(null, "Ligar para o candidato: " + candidato);
+            } else if (valorDesejado < 2000) {
+                JOptionPane.showMessageDialog(null, "Ligar para " + candidato + " com contra proposta");
+            } else {
+                JOptionPane.showMessageDialog(null, "Aguardando resultado dos demais candidatos");
+            }
 
-        if (n2 >= 2000) {
-            JOptionPane.showMessageDialog(null, "Ligar para o Candidato");
-        } else if (n2 <= 2000) {
-            JOptionPane.showMessageDialog(null, "Ligar para Candidato com Contra Proposta");
-        } else {
-            JOptionPane.showMessageDialog(null, "Aguardando Resultado dos Demais Candidatos");
+            JOptionPane.showMessageDialog(null, "Ligando para " + candidato + "...");
+            JOptionPane.showMessageDialog(null, "Ligação realizada com sucesso.");
         }
-
     }
 }
